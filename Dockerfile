4d2a531e4b7593e4d31b8453e77f48a24ee38f4b
Pablo Perez <pablo@tappx.com>
2019-01-17 21:43:18 +0100

    Adding welcome page and improving dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM golang:latest
 
 WORKDIR /go/src/app
-ADD . .
 RUN go get "gopkg.in/mgo.v2/bson" && \
 	go get "gopkg.in/mgo.v2" && \
 	go get "github.com/tidwall/gjson" && \
-	go get "github.com/gorilla/mux" && \
-	go build -o /go/bin/biwenger-api .
+	go get "github.com/gorilla/mux"
+ADD . .
+RUN go build -o /go/bin/biwenger-api .
 
 EXPOSE 8080
 ENTRYPOINT /go/bin/biwenger-api
--- router.go
+++ router.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "log"
+    "fmt"
     "net/http"
     "github.com/gorilla/mux"
 )
@@ -9,11 +10,16 @@ import (
 
 func main() {
     router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", sayHi)
     router.HandleFunc("/login", login)
     router.HandleFunc("/getMyPlayers", GetMyPlayers)
     router.HandleFunc("/getPlayerById", GetPlayerById)
     router.HandleFunc("/setLineUp", SetLineUp)
     log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func sayHi(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi from your biwenger api!!")
 }
 
 
