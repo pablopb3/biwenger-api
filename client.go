f01065a77305c98ea96f688550501f73f9442523
charly3pins <charlyfb90@gmail.com>
2019-10-01 23:39:53 +0200

    client for connect to biwenger api

---
+++ client.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+
+	"github.com/magiconair/properties"
+)
+
+func newClient(apiURL string) *client {
+	return &client{
+		apiURL: apiURL,
+	}
+}
+
+type client struct {
+	apiURL string
+}
+
+type request struct {
+	endpoint string
+	method   string
+	body     []byte
+	token    string
+}
+
+func (c *client) doRequest(r request) ([]byte, error) {
+	req, err := http.NewRequest(r.method, c.apiURL+r.endpoint, bytes.NewBuffer(r.body))
+	if err != nil {
+		log.Println("error creating new request", err)
+		return nil, err
+	}
+
+	// TODO improve this check
+	if r.endpoint != loginURL {
+		// TODO move it to init()
+		p := properties.MustLoadFile("application.properties", properties.UTF8)
+		req.Header.Set("authorization", "Bearer "+r.token)
+		req.Header.Set("x-lang", "en")
+		req.Header.Set("x-league", p.GetString("leagueId", ""))
+		req.Header.Set("x-user", p.GetString("userId", ""))
+		req.Header.Set("x-version", p.GetString("biwengerVersion", ""))
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Println("error executing request", err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("error reading body from response", err)
+		return nil, err
+	}
+
+	return body, nil
+}
