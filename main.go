1ecd94d30227aee46405484a9a86836874f6f1bb
charly3pins <charlyfb90@gmail.com>
2019-10-09 11:53:09 +0200

    inject db, create client, group endpoints

--- main.go
+++ main.go
@@ -1,47 +1,61 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
 )
 
 const baseURL = "https://biwenger.as.com/api/v2"
 
 func main() {
-	cli := newClient(baseURL)
+	uri := flag.String("mongo", "mongodb://mongodb:27017", "mongodb connection address")
+	mongodb := flag.String("db", "biwenger", "name of the mongodb database")
+	collection := flag.String("collection", "players", "collection for players")
+	flag.Parse()
+
+	sess, err := mgo.Dial(*uri)
+	if err != nil {
+		log.Fatalf("Can't connect to mongo: %s \n", err.Error())
+	}
+	defer sess.Close()
+
+	col := sess.DB(*mongodb).C(*collection)
+	s := Store{c: col}
+	cli := Client{apiURL: baseURL}
 
 	router := gin.Default()
-	v1 := router.Group("/v1")
 
-	v1.GET("/ping", pong)
-	v1.POST("/login", cli.login)
-	v1.GET("/getDaysToNextRound", cli.getDaysToNextRound)
+	router.GET("/greet", greet)
 
-	// router.HandleFunc("/"+v+"/getDaysToNextRound", GetDaysToNextRound)
-	// router.HandleFunc("/"+v+"/getMyPlayers", GetMyPlayers)
-	// router.HandleFunc("/"+v+"/getPlayerById", GetPlayerById)
-	// router.HandleFunc("/"+v+"/setLineUp", SetLineUp)
+	router.POST("/login", cli.login)
 
-	// router.HandleFunc("/"+v+"/sendPlayersToMarket", SendPlayersToMarket)
-	// router.HandleFunc("/"+v+"/getPlayersInMarket", GetPlayersInMarket)
+	squad := router.Group("/squad")
+	squad.GET("", cli.myPlayers)
+	squad.PUT("/lineup", cli.setLineUp)
+	squad.GET("/money", cli.GetMyMoney)
+	squad.GET("/maxbid", cli.GetMaxBid)
 
-	// router.HandleFunc("/"+v+"/getReceivedOffers", GetReceivedOffers)
-	// router.HandleFunc("/"+v+"/acceptReceivedOffer", AcceptReceivedOffer)
-	// router.HandleFunc("/"+v+"/placeOffer", PlaceOffer)
+	market := router.Group("/market")
+	market.GET("/evolution", cli.GetMarketEvolution)
+	market.PUT("/squad", cli.SendPlayersToMarket)
+	market.PUT("/squad/:playerID", nil) // For the future
+	market.GET("/players", cli.GetPlayersInMarket)
+	market.POST("/offers", cli.PlaceOffer)
+	market.GET("/offers", cli.GetReceivedOffers)
+	market.PUT("/offers/:id", cli.AcceptOffer)
 
-	// router.HandleFunc("/"+v+"/getMyMoney", GetMyMoney)
-	// router.HandleFunc("/"+v+"/getMaxBid", GetMaxBid)
-	// router.HandleFunc("/"+v+"/getMarketEvolution", GetMarketEvolution)
+	router.GET("/players/:id", cli.getPlayer(s))
+	router.PUT("/players", cli.updatePlayers(s))
 
-	// router.HandleFunc("/"+v+"/updatePlayersAlias", UpdatePlayersAliasInDb)
-
-	// router.HandleFunc("/"+v+"/tweet", Tweet)
+	router.GET("/rounds/next", cli.getDaysToNextRound)
 
 	log.Fatal(router.Run(":8080"))
 }
 
-func pong(c *gin.Context) {
-	c.JSON(http.StatusOK, "pong")
+func greet(c *gin.Context) {
+	c.JSON(http.StatusOK, "Hi from your biwenger api!!")
 }
